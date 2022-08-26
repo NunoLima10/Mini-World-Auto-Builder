318ac91797d7d6531fbaff94602edc5925ae3ce5
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2022-08-26 17:07:57 -0100

    fead: add tutorial link

--- src/browser.py
+++ src/browser.py
@@ -3,7 +3,7 @@ import webbrowser
 class Browser:
     def __init__(self) -> None:
         self.urls = {
-            "Tutorial":"https://www.youtube.com/c/NunoLima10",
+            "Tutorial":"https://www.youtube.com/watch?v=cMcZrRz0lRA&list=PLLcS6ldQh_lwxzNRjsAgwjVg_CcOJsxFd&index=2",
             "Online Voxelizer":"https://drububu.com/miscellaneous/voxelizer/?out=obj",
             "YouTube Channel":"https://www.youtube.com/c/NunoLima10",
             "Repository":"https://github.com/NunoLima10/Mini-World-Auto-Builder",
